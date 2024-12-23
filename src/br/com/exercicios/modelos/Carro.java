4212786640ae9f624c3781ba5b1f88196e7f5201
Jean <jeanmatheus0612@gmail.com>
2024-12-23 20:23:44 -0300

    Criando os atributos e a subclasse

--- src/br/com/exercicios/modelos/Carro.java
+++ src/br/com/exercicios/modelos/Carro.java
@@ -1,5 +1,17 @@
 package br.com.exercicios.modelos;
 
 public class Carro {
+    private String nomeDoModelo;
+    private double precoAno1;
+    private double precoAno2;
+    private double precoAno3;
+
     
+    public String getNomeDoModelo() {
+        return nomeDoModelo;
+    }
+
+    
+
+
 }
