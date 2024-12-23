7bda6750610ebf75dadf978901ac28ff56915acf
Jean <jeanmatheus0612@gmail.com>
2024-12-23 20:32:54 -0300

    Criando metodos getters e setters na classe Carro

--- src/br/com/exercicios/modelos/Carro.java
+++ src/br/com/exercicios/modelos/Carro.java
@@ -6,12 +6,32 @@ public class Carro {
     private double precoAno2;
     private double precoAno3;
 
-    
+
     public String getNomeDoModelo() {
         return nomeDoModelo;
     }
+    public void setNomeDoModelo(String nomeDoModelo) {
+        this.nomeDoModelo = nomeDoModelo;
+    }
+    public double getPrecoAno1() {
+        return precoAno1;
+    }
+    public void setPrecoAno1(double precoAno1) {
+        this.precoAno1 = precoAno1;
+    }
+    public double getPrecoAno2() {
+        return precoAno2;
+    }
+    public void setPrecoAno2(double precoAno2) {
+        this.precoAno2 = precoAno2;
+    }
+    public double getPrecoAno3() {
+        return precoAno3;
+    }
+    public void setPrecoAno3(double precoAno3) {
+        this.precoAno3 = precoAno3;
+    }
 
-    
 
 
 }
