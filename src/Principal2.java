3a0e8a846aef115d77b1012d5c131b68d59c8db1
Jean <jeanmatheus0612@gmail.com>
2024-12-23 22:44:50 -0300

    Criando os atributos do Animal

--- src/Principal2.java
+++ src/Principal2.java
@@ -1,3 +1,13 @@
+import br.com.exercicios.modelos2.Cachorro;
+import br.com.exercicios.modelos2.Gato;
+
 public class Principal2 {
-    
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+
+
+
+
+        Gato gato = new Gato();
+    }
 }
--- src/br/com/exercicios/modelos2/Animal.java
+++ src/br/com/exercicios/modelos2/Animal.java
@@ -1,6 +1,10 @@
 package br.com.exercicios.modelos2;
 
 public class Animal {
+    private String nome;
+    private int idade;
+    private String raca;
+    private String cor;
     
     public void emitirSom() {
         System.out.println("O animal está emitindo um som");
