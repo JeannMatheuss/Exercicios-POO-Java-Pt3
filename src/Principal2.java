03ed6338cd0122a65ecea6aaaad7536211b95520
Jean <jeanmatheus0612@gmail.com>
2024-12-23 22:50:08 -0300

    Adicionando valores ao cachorro

--- src/Principal2.java
+++ src/Principal2.java
@@ -4,9 +4,10 @@ import br.com.exercicios.modelos2.Gato;
 public class Principal2 {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro();
-        
-
-
+        cachorro.setNome("Trovão Uchiha");
+        cachorro.setIdade(3);
+        cachorro.setCor("Preto");
+        cachorro.setRaca("Golden");
 
 
         Gato gato = new Gato();
