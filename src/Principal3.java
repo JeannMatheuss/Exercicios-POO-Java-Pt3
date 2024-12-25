fb9d7751d76fa1e2bbdebe701bb25044731c99fc
Jean <jeanmatheus0612@gmail.com>
2024-12-24 23:31:13 -0300

    Atribuindo valores ao programa

--- src/Principal3.java
+++ src/Principal3.java
@@ -1,5 +1,19 @@
+import br.com.exercicios.modelos3.ContaBancaria;
+import br.com.exercicios.modelos3.ContaCorrente;
+
 public class Principal3 {
     public static void main(String[] args) {
-        
+        // Criando uma conta bancária com saldo inicial de 1000
+        ContaBancaria conta = new ContaBancaria();
+        conta.depositar(1000);
+        conta.consultarSaldo();
+
+        // Realizando operações na conta corrente
+        ContaCorrente contaCorrente = new ContaCorrente();
+        contaCorrente.depositar(200);
+        contaCorrente.cobrarTarifaMensal();
+        contaCorrente.consultarSaldo();
+        contaCorrente.sacar(150);
+        contaCorrente.consultarSaldo();
     }
 }
