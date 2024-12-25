970f0aac9af8728d1b80d13168f26b6e87915b15
Jean <jeanmatheus0612@gmail.com>
2024-12-25 17:57:57 -0300

    Atribuindo valores

--- src/Principal4.java
+++ src/Principal4.java
@@ -1,3 +1,17 @@
+import br.com.exercicios.modelos4.GeradorPrimo;
+import br.com.exercicios.modelos4.NumerosPrimos;
+import br.com.exercicios.modelos4.VerificadorPrimo;
+
 public class Principal4 {
-    
+    public static void main(String[] args) {
+        VerificadorPrimo verificador = new VerificadorPrimo();
+        verificador.verificarSeEhPrimo(17);
+
+        GeradorPrimo gerador = new GeradorPrimo();
+        int proximoPrimo = gerador.gerarProximoPrimo(17);
+        System.out.println("O próximo primo após 17 é: " + proximoPrimo);
+
+        NumerosPrimos numerosPrimos = new NumerosPrimos();
+        numerosPrimos.listarPrimos(30);
+    }
 }
